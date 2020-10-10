f7b261f967897eaa508256e3321ff521a755429f
Sharad Shukla <b17061@students.iitmandi.ac.in>
2020-10-11 00:44:35 +0530

    Lab 2, Part A, Question 2.5, Add NEGSUPPORT: in every printf statement

--- sqrt.c
+++ sqrt.c
@@ -10,24 +10,24 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("NEGSUPPORT: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 	int input = atoi(argv[1]);
 	if(input < 0)
 	{
 		input = (-1) * input;
-		printf("Sqrt of -%d is %fi\n",input,sqrt(input));
+		printf("NEGSUPPORT: Sqrt of -%d is %fi\n",input,sqrt(input));
 	}
 	else if(isdigit(argv[1][0]))
 	{
-		printf("Sqrt of %d is %f\n",input,sqrt(input));
+		printf("NEGSUPPORT: Sqrt of %d is %f\n",input,sqrt(input));
 	}
 	else
 	{
-		printf("Not A Number\n");
+		printf("NEGSUPPORT: Not A Number\n");
 	}
-	printf("End of program. Exiting.\n");
+	printf("NEGSUPPORT: End of program. Exiting.\n");
 	return(0);
 
 } // end main
